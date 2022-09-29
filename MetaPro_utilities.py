401688415301f11efb4596a250580d7839c5dba7
billy <billyc59@gmail.com>
2022-09-29 13:10:31 -0400

    bug with statecheck at GA final merge
    now waits for the job to be done properly

--- MetaPro_utilities.py
+++ MetaPro_utilities.py
@@ -265,6 +265,8 @@ class mp_util:
                 else:
                     self.wait_for_mp_store()
             time.sleep(job_delay)
+        #final wait for everything to be done
+        self.wait_for_mp_store()
                 
         
     def launch_only_with_hold(self, mem_threshold, job_limit, job_delay, job_name, command_obj, command):
