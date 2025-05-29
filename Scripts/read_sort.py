e163c6938aed6bfc678770e0a3feba40eb89b66a
billy <billyc59@gmail.com>
2025-05-29 00:41:56 -0400

    patched read_sort

--- Scripts/read_sort.py
+++ Scripts/read_sort.py
@@ -1,16 +1,19 @@
 import sys
 import time
 import pandas as pd
+import os
+from datetime import datetime as dt
+import numpy as np
 
 # This code started off as a testbed to develop a method to import FASTQs effectively into pandas.
 # It simply imports the FASTQ file, sorts by the ID, and exports it back. 
 
-def sort_and_export(input_file, output_file, direction):
+def sort_and_export(input_file, output_file):
 
-    start_total_call = time.clock()
+    start_total_call = time.perf_counter()
     print("input file:", input_file)
-    df = pd.read_csv(input_file, header=None, names=[None], sep='\n', quoting=3, skip_blank_lines = False)
-    end_read_time = time.clock()
+    df = pd.read_csv(input_file, header=None, names=[None], sep=r'\n', engine = "python", quoting=3, skip_blank_lines = False)
+    end_read_time = time.perf_counter()
     
     # reshaping, aka: unflattening the 1-D array into something meaningful to us
     # we've chosen to leave the @ in the ID.  we didn't feel a strong need to strip it away.
@@ -21,9 +24,10 @@ def sort_and_export(input_file, output_file, direction):
     df = df.sort_values(by=['ID'])
     
         
-    end_df_time = time.clock()
-    df.to_csv(output_file, sep='\n', mode = 'w+', header=False, index=False, quoting = 3)
-    end_total_call = time.clock()
+    end_df_time = time.perf_counter()
+    np.savetxt(output_file, df.values.flatten(), fmt='%s')
+    #df.to_csv(output_file, sep='\n', mode = 'w+', header=False, index=False, quoting = 3)
+    end_total_call = time.perf_counter()
     #-------------------------------------------------------------------------------------------
     
     print("import csv time:", end_read_time - start_total_call, "s")
@@ -33,13 +37,10 @@ def sort_and_export(input_file, output_file, direction):
 
     
 if __name__ == "__main__":
-    scinet_flag = False
-    if(len(sys.argv) != 4):
-        print("something wrong with the args")
-        sys.exit()
-    else:
-        input_fastq_path = sys.argv[1]      # expects a path to the fastq file, including the name
-        export_path = sys.argv[2]           # expects a place to dump the new fastq, including the name
-        direction = sys.argv[3]
+    print(dt.today(), "START TIME")
+    input_fastq_path = os.path.abspath(sys.argv[1])      # expects a path to the fastq file, including the name
+    export_path = os.path.abspath(sys.argv[2])           # expects a place to dump the new fastq, including the name
     
-    sort_and_export(input_fastq_path, export_path, direction)
+    
+    sort_and_export(input_fastq_path, export_path)
+    print(dt.today(), "DONE")
